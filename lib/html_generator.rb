6ecef228d64bd4efbfc4e526488baa65449ddce5
Luke McComb <lucasmccomb@gmail.com>
2014-06-25 10:31:43 -0600

    Got 1st, 2nd and 3rd test to pass

--- lib/html_generator.rb
+++ lib/html_generator.rb
@@ -1,5 +1,27 @@
 require "erb_generator"
 
 class HTMLGenerator < ERBGenerator
- 
+  def section(text)
+    template = "<section><%= section_text %></section>"
+    options_hash = {:locals => {:section_text => text}}
+    erb template, options_hash
+  end
+
+  def unordered_list(list)
+    template = <<-TEMPLATE
+      <ul>
+        <% items.each do |item| %>
+          <li><%= item %></li>
+        <% end %>
+      </ul>
+    TEMPLATE
+    options_hash = {:locals => {:items => list}}
+    erb template, options_hash
+  end
+
+  def button(text)
+    template = "<button><%= button_text %></button>"
+    options_hash = {:locals => {:button_text => text}}
+    erb template, options_hash
+  end
 end
